dfd33630c642bf90f9fcf70b6218022a1e68dc47
mraffiramdhani <raffi.programming@gmail.com>
2019-12-23 21:36:46 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// import express, cors, body-parser, dotenv
+require('dotenv').config()
+const express = require('express'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    // { auth } = require('./src/middleware'),
+    port = process.env.APP_PORT;
+
+// assign express instance to app variable
+const app = express();
+
+// assign express middleware (body-parser)
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+app.use(cors())
+
+// define app routes
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+// define app port
+app.listen(port, () => {
+    console.log('App Listen on Port ' + port)
+});
