19a0f26704c20e7c1ceff426fde29fcba6980ee8
mraffiramdhani <raffi.programming@gmail.com>
2019-12-26 14:56:27 +0700

    Implement Asyncronous on Cart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -2,71 +2,42 @@
 
 var Cart = require('../models/cart')
 
-module.exports.list_user_cart = (req, res) => {
+module.exports.list_user_cart = async (req, res) => {
     const { id } = req.headers['auth_token']
 
-    Cart.getCartByUserId(id, (err, result) => {
-        console.log('Cart Controller Cart index')
-        if (err) {
-            res.send(err)
-            console.log('error', err)
-            console.log('res', result)
-        } else {
-            res.send({
-                success: true,
-                result
-            })
-        }
+    const data = await Cart.getCartByUserId(id)
+    res.send({
+        success: true,
+        data
     })
 }
 
-module.exports.add_item_to_cart = (req, res) => {
+module.exports.add_item_to_cart = async (req, res) => {
     const { id } = req.headers['auth_token']
-    Cart.addItemtoCart(id, new Cart(req.body), (err, result) => {
-        console.log('Cart Controller add item to user cart')
-        if (err) {
-            res.send(err)
-            console.log('error', err)
-            console.log('res', result)
-        } else {
-            res.send({
-                success: true,
-                result
-            })
-        }
+
+    const data = await Cart.addItemtoCart(id, new Cart(req.body))
+    res.send({
+        success: true,
+        data
     })
 }
 
-module.exports.update_item_in_cart = (req, res) => {
+module.exports.update_item_in_cart = async (req, res) => {
     const { id } = req.params
-    Cart.updateIteminCart(id, new Cart(req.body), (err, result) => {
-        console.log('Cart Controller update item in user cart')
-        if (err) {
-            res.send(err)
-            console.log('error', err)
-            console.log('res', result)
-        } else {
-            res.send({
-                success: true,
-                result
-            })
-        }
+
+    const data = await Cart.updateIteminCart(id, new Cart(req.body))
+    res.send({
+        status: true,
+        data
     })
 }
 
-module.exports.delete_item_in_cart = (req, res) => {
+module.exports.delete_item_in_cart = async (req, res) => {
     const { id } = req.params
-    Cart.deleteIteminCart(id, (err, result, fields) => {
-        console.log('Cart Controller delete item in cart')
-        if (err) {
-            res.send(err)
-            console.log('error', err)
-            console.log('res', result)
-        } else {
-            res.send({
-                success: true,
-                result
-            })
-        }
+
+    const data = await Cart.deleteIteminCart(id)
+    res.send({
+        status: true,
+        data
     })
 }
--- src/models/cart.js
+++ src/models/cart.js
@@ -10,56 +10,49 @@ var Cart = function Cart(cart) {
     this.updated_at = new Date()
 }
 
-Cart.getCartByUserId = (userId, result) => {
+Cart.getCartByUserId = (userId) => {
     return new Promise((resolve, reject) => {
         conn.query('select * from carts where user_id=?', userId, (err, res, fields) => {
             if (err) reject(err)
-            resolve(result(res))
+            resolve(res)
         })
     })
 }
 
-Cart.addItemtoCart = (userId, newItem, result) => {
+Cart.addItemtoCart = (userId, newItem) => {
     const { item_id, quantity, description, created_at, updated_at } = newItem
-    conn.query('insert into carts(item_id, quantity, description, user_id, created_at, updated_at) values(?,?,?,?,?,?)',
-        [item_id, quantity, description, userId, created_at, updated_at],
-        (err, res, fields) => {
-            if (err) {
-                console.log('error: ', err)
-                result(null, err)
-            } else {
-                console.log('data:', res)
-                result(null, res)
-            }
-        })
+
+    return new Promise((resolve, reject) => {
+        conn.query('insert into carts(item_id, quantity, description, user_id, created_at, updated_at) values(?,?,?,?,?,?)',
+            [item_id, quantity, description, userId, created_at, updated_at],
+            (err, res, fields) => {
+                if (err) reject(err)
+                resolve(res)
+            })
+    })
 }
 
-Cart.updateIteminCart = (id, item, result) => {
+Cart.updateIteminCart = (id, item) => {
     const { quantity, description, updated_at } = item
-    conn.query('update carts set quantity=?, description=?, updated_at=? where id=?',
-        [quantity, description, updated_at, id],
-        (err, res, fields) => {
-            if (err) {
-                console.log('error: ', err)
-                result(null, err)
-            } else {
-                console.log('data:', res)
-                result(null, res)
-            }
-        })
+
+    return new Promise((resolve, reject) => {
+        conn.query('update carts set quantity=?, description=?, updated_at=? where id=?',
+            [quantity, description, updated_at, id],
+            (err, res, fields) => {
+                if (err) reject(err)
+                resolve(res)
+            })
+    })
 }
 
-Cart.deleteIteminCart = (id, result) => {
-    conn.query('delete from carts where id=?',
-        id, (err, res, fields) => {
-            if (err) {
-                console.log('error: ', err)
-                result(null, err)
-            } else {
-                console.log('data:', res)
-                result(null, res)
-            }
-        })
+Cart.deleteIteminCart = (id) => {
+    return new Promise((resolve, reject) => {
+        conn.query('delete from carts where id=?',
+            id, (err, res, fields) => {
+                if (err) reject(err)
+                resolve(res)
+            })
+    })
 }
 
 module.exports = Cart
