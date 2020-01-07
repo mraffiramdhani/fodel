566e880112ebf1c683716445e0bf2c5e92e0b76e
mraffiramdhani <raffi.programming@gmail.com>
2020-01-07 20:14:29 +0700

    Middleware Update

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken'),
-    conn = require('./dbconfig');
+    conn = require('./dbconfig'),
+    { response } = require('./helper/response');
 
 const auth = (req, res, next) => {
     if (req.headers['authorization'] && req.headers['authorization'].startsWith('Bearer')) {
@@ -7,35 +8,23 @@ const auth = (req, res, next) => {
         req.headers['jwt_token'] = jwt_token
         conn.execute('select token from revoked_token where token=? and is_revoked=1', [jwt_token], (err, result, fields) => {
             if (err) {
-                res.send({
-                    success: false,
-                    message: err
-                })
+                response(res, 200, false, "Error.", err)
             } else {
                 if (result.length > 0) {
-                    res.send({
-                        success: false,
-                        message: "Session Expired. Please Log In Again"
-                    })
+                    response(res, 200, false, "Session Expired. Please Log In Again.")
                 } else {
                     try {
                         const auth_data = jwt.verify(jwt_token, process.env.APP_KEY)
                         req.auth = auth_data
                         next()
                     } catch (e) {
-                        res.send({
-                            success: false,
-                            message: e
-                        })
+                        response(res, 200, false, "Error.", e)
                     }
                 }
             }
         })
     } else {
-        res.send({
-            success: false,
-            message: "You must be login first"
-        })
+        response(res, 200, false, "Authorization Failed. Please Log In Again.")
     }
 }
 
@@ -45,10 +34,7 @@ const hasRole = function HasRole(roles) {
         const { role_id } = req.auth
         conn.execute('select * from roles where id=?', [role_id], (err, result) => {
             if (err) {
-                res.send({
-                    success: false,
-                    message: "Access Denied. Error : " + err
-                })
+                response(res, 200, false, "Error.", err)
             } else if (result.length > 0) {
                 if (Array.isArray(roles)) {
                     var checked = false
@@ -70,18 +56,12 @@ const hasRole = function HasRole(roles) {
                     if (is_auth) {
                         next()
                     } else {
-                        res.send({
-                            success: false,
-                            message: "Access Denied. User Role Unidentified"
-                        })
+                        response(res, 200, false, "Access Denied. User Role Unidentified.")
                     }
                 } else if (typeof roles == 'string' && roles.toLowerCase() == result[0].name) {
                     next()
                 } else {
-                    res.send({
-                        success: false,
-                        message: "Access Denied. User Role Unidentified"
-                    })
+                    response(res, 200, false, "Access Denied. User Role Unidentified.")
                 }
             }
         })
